6dacea9e56d3bc4e76ce409b76fb4f48cabe4a86
DESKTOP-C4Q78DI <hasnainrakha58@gmail.com>
2022-04-03 19:57:13 +0500

    4apr

--- routes/products.js
+++ routes/products.js
@@ -22,6 +22,43 @@ router.post('/',(req,res) => {
         res.status(500).send("Product not added")
     });
 });
+// get all products
+router.get("/",(req,res) =>{
+    Product.find().then((products) => res.send(products))
+    .catch((error) => {
+        res.status(500).send("Something went wrong");
+    });
+});
+router.get("/:productId", async (req, res) => {
+    const product = await Product.findById(req.params.productId);
+
+    
+    if(!product) 
+    res.status(404).send("Product not found");
+    else
+    res.send(product);
+    
+});
+//Update product based on ID
+router.put('/:productId', async (req, res) => {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.productId,
+        {
+        name: req.body.productName,
+        price: req.body.price,
+        color: req.body.color,
+        producttype: req.body.producttype,
+        description: req.body.description,
+
+
+
+    },
+    { new: true});
+    if(!updatedProduct) 
+    res.status(404).send("Product not found");
+    else
+    res.send(updatedProduct);
+});
+
 
 module.exports = router;
 
